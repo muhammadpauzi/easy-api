2b2170d5bb8fb8f27ec7d721ae1a131ac8c4984c
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-02-05 17:35:39 +0700

    fix: register route in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import App from './App';
-import "reflect-metadata";
+import 'reflect-metadata';
 import express from 'express';
+
+import authRouter from './routes/auth';
 
 const app = new App();
 
-app
-    .registerMiddleware(express.json())
+app.registerMiddleware(express.json())
+    .registerRoute('/auth', authRouter)
     .run(5000, (port: number) => {
-        console.log("Database connected!");
+        console.log('Database connected!');
         console.log(`Server is running at http://127.0.0.1:${port}`);
-    })
-
+    });
