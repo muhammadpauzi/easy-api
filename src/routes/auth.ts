3da5ec5359f92e688fbc2f899e0b91c21e9ada29
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-02-05 17:34:04 +0700

    fix: method of login, register and logout endpoint

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,8 +5,8 @@ const router = Router();
 const authController = new AuthController();
 
 router.get('/user', (req, res) => authController.user(req, res));
-router.get('/login', (req, res) => authController.login(req, res));
-router.get('/register', (req, res) => authController.register(req, res));
-router.get('/logout', (req, res) => authController.logout(req, res));
+router.post('/login', (req, res) => authController.login(req, res));
+router.post('/register', (req, res) => authController.register(req, res));
+router.delete('/logout', (req, res) => authController.logout(req, res));
 
 export default router;
