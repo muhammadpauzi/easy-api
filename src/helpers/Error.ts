0873f80070bb0c04d4ae01169b05352b1bb917b8
muhammadpauzi <muhammadpauzi200@gmail.com>
2022-02-05 16:08:51 +0700

    fix: AuthMiddleware, error handling and interface to get user from Request(express)

--- src/helpers/Error.ts
+++ src/helpers/Error.ts
@@ -2,9 +2,9 @@ import { Response } from "express";
 import ApiResponse from "./ApiResponse";
 
 export default class Error {
-    public static handleError(res: Response, error: any) {
+    public static handleError(res: Response, error: any, statusCode: number = 500) {
         console.log(error);
         const { code, message } = error;
-        return ApiResponse.errorResponse(res, code, { code, message });
+        return ApiResponse.errorResponse(res, statusCode, { errorCode: code, message });
     }
 }
--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -1,87 +1,108 @@
-import jwt, {TokenExpiredError} from 'jsonwebtoken';
-import { NO_TOKEN_PROVIDED, NO_USER_WITH_THE_TOKEN, SERVER_ERROR, THE_USER_ALREADY_LOGGED_IN, THE_USER_ALREADY_LOGGED_OUT, TOKEN_NOT_VALID } from '../constants/messages';
-import User from '../entities/User';
-import ApiResponse from '../helpers/ApiResponse';
+import {
+    TokenExpiredError,
+    verify,
+    Secret,
+    JwtPayload,
+    JsonWebTokenError,
+} from 'jsonwebtoken'
+import { Request, Response, NextFunction } from 'express'
+import {
+    NO_TOKEN_PROVIDED,
+    NO_USER_WITH_THE_TOKEN,
+    SERVER_ERROR,
+    THE_USER_ALREADY_LOGGED_IN,
+    THE_USER_ALREADY_LOGGED_OUT,
+    TOKEN_NOT_VALID,
+} from '../constants/messages'
+import User from '../entities/User'
+import ApiResponse from '../helpers/ApiResponse'
+import Error from '../helpers/Error'
+import { IGetUserAuthInfoRequest } from '../interfaces/IGetUserAuthInfoRequest'
 
 export default class AuthMiddleware {
-    public async verifyJwtToken(req, res, next) {
+    public async verifyJwtToken(
+        req: IGetUserAuthInfoRequest,
+        res: Response,
+        next: NextFunction
+    ) {
         try {
-          if (!req.cookies.token)
-            return ApiResponse.errorUnauthorizedResponse(res, { message: NO_TOKEN_PROVIDED });
-
-            const { token: jwtToken } = req.cookies;
-          jwt.verify(
-            jwtToken,
-            process.env.JWT_SECRET_KEY,
-            async (err, decodedJwtToken) => {
-              if (err instanceof TokenExpiredError) {
-                return ApiResponse.errorUnauthorizedResponse(res, { message: NO_TOKEN_PROVIDED });
-              }
-      
-              if (err) {
-                console.log(err);
-                return ApiResponse.errorUnauthorizedResponse(res, { message: TOKEN_NOT_VALID });
-              }
-      
-              const user = await User.findOne({ where: {
-                  id: decodedJwtToken.user.id
-              } });
-      
-              if (!user)
-                return ApiResponse.errorResponse(res, 404, { message: NO_USER_WITH_THE_TOKEN });
-      
-              if (!user.sessionId)
+            // get token from cookie
+            if (!req.cookies.token)
                 return ApiResponse.errorUnauthorizedResponse(res, {
-                  message: THE_USER_ALREADY_LOGGED_OUT,
-                });
-      
-              req.user = {
+                    message: NO_TOKEN_PROVIDED,
+                })
+            const { token: jwtToken } = req.cookies
+
+            const decodedToken: JwtPayload = <JwtPayload>(
+                verify(jwtToken, <Secret>process.env.JWT_SECRET_KEY)
+            )
+
+            const user = await User.findOne({
+                where: {
+                    id: decodedToken.user.id,
+                },
+            })
+
+            if (!user)
+                return ApiResponse.errorResponse(res, 404, {
+                    message: NO_USER_WITH_THE_TOKEN,
+                })
+
+            if (!user.sessionId)
+                return ApiResponse.errorUnauthorizedResponse(res, {
+                    message: THE_USER_ALREADY_LOGGED_OUT,
+                })
+
+            req.user = {
                 id: user.id,
                 username: user.username,
-              };
-              next();
             }
-          );
+            next()
         } catch (error) {
-          console.log(error);
-          return ApiResponse.errorResponse(res, 500, {
-            message: SERVER_ERROR,
-            error_message: error.message,
-          });
+            console.log(error)
+            if (error instanceof TokenExpiredError)
+                return ApiResponse.errorUnauthorizedResponse(res, {
+                    message: NO_TOKEN_PROVIDED,
+                })
+            if (error instanceof JsonWebTokenError)
+                return ApiResponse.errorUnauthorizedResponse(res, {
+                    message: TOKEN_NOT_VALID,
+                })
+            return Error.handleError(res, error)
         }
-      };
+    }
 
-      public async blockLoggedInUser(req, res, next) {
+    public async blockLoggedInUser(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) {
         try {
-          if (!req.cookies.token) return next();
-      
-          const { token: jwtToken } = req.cookies;
-          jwt.verify(
-            jwtToken,
-            process.env.JWT_SECRET_KEY,
-            async (err, decodedJwtToken) => {
-              if (err) {
-                console.log(err);
-                return next();
-              }
-      
-              const user = await User.findOne({ where: {
-                  id: decodedJwtToken.user.id
-              } });
-              if (user && user?.sessionId) {
-                return ApiResponse.errorResponse(res, 403, {
-                  message: THE_USER_ALREADY_LOGGED_IN,
-                });
-              }
-              return next(); // user not exists on db or jwt error must be run next() function
-            }
-          );
+            if (!req.cookies.token) return next()
+
+            const { token: jwtToken } = req.cookies
+            try {
+                const decodedToken: JwtPayload = <JwtPayload>(
+                    verify(jwtToken, <Secret>process.env.JWT_SECRET_KEY)
+                )
+
+                const user = await User.findOne({
+                    where: {
+                        id: decodedToken.user.id,
+                    },
+                })
+
+                if (user && user?.sessionId) {
+                    return ApiResponse.errorResponse(res, 403, {
+                        message: THE_USER_ALREADY_LOGGED_IN,
+                    })
+                }
+                return next() // user not exists on db or jwt error must be run next() function
+            } catch (error) {}
         } catch (error) {
-          console.log(error);
-          return ApiResponse.errorResponse(res, 500, {
-            message: SERVER_ERROR,
-            error_message: error.message,
-          });
+            console.log(error)
+            if (error instanceof JsonWebTokenError) return next()
+            return Error.handleError(res, error)
         }
-      };
+    }
 }
